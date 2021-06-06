ae36f4e91b01a17a74374191e09363a5615c0f23
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-06-06 16:01:01 +0300

    finished_hw

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FrendListItem from "./FriendListItem";
+import styles from "./friendList.module.css";
+
+const FriendList = ({ friends }) => (
+  <ul className={styles.friend_list}>
+    {friends.map((friend) => (
+      <FrendListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
